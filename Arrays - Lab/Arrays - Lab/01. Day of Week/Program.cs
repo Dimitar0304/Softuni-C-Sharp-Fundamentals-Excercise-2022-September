d91350126136c9c939b8b19df9e09e4270e2ee02
DimitarBanchev <114139864+DimitarBanchev@users.noreply.github.com>
2022-10-16 13:37:47 +0300

    Add files via upload

---
+++ Arrays - Lab/Arrays - Lab/01. Day of Week/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _01._Day_of_Week
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = int.Parse(Console.ReadLine());
+            string[] daysOfWeek = { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
+            if (input <=0 || input > 7)
+            {
+                Console.WriteLine("Invalid day!");
+            }
+            else
+            {
+                Console.WriteLine(daysOfWeek[input -1]);
+            }
+        }
+    }
+}
---
+++ Arrays - Lab/Arrays - Lab/2. Print Numbers in Reverse Order/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _2._Print_Numbers_in_Reverse_Order
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+           
+            int[] num = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+              int  nums = int.Parse(Console.ReadLine());
+                num[i] = nums;
+
+            }
+            for (int i = num.Length - 1; i >= 0; i--)
+            {
+                Console.Write(num[i]+" " );
+            }
+        }
+    }
+}
