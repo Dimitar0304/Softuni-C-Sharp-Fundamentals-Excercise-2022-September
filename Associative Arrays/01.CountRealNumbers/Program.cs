90876b898142d5c77b90a2a1b555cc14ca200473
DimitarBanchev <114139864+DimitarBanchev@users.noreply.github.com>
2022-12-16 10:30:11 +0200

    Add files via upload

---
+++ Associative Arrays/01.CountRealNumbers/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.CountRealNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedDictionary<double, int> counts = new SortedDictionary<double, int>();
+
+            double[] numbers = Console.ReadLine().Split().Select(double.Parse).ToArray();
+
+            foreach (var item in numbers)
+            {
+                if (counts.ContainsKey(item))
+                {
+                    counts[item]++;
+                }
+                else
+                {
+                    counts.Add(item, 1);
+                }
+            }
+            foreach (var count in counts)
+            {
+                Console.WriteLine($"{counts.Keys} -> {counts.Values}");
+            }
+        }
+    }
+}
