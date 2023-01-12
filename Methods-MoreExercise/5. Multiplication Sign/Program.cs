ddbc7b7092bdfe1a837f27b57e7807a29adf96a4
Димитър Банчев <114139864+DimitarBanchev@users.noreply.github.com>
2023-01-12 23:36:41 +0200

    Add files via upload

---
+++ Methods-MoreExercise/5. Multiplication Sign/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _5._Multiplication_Sign
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
